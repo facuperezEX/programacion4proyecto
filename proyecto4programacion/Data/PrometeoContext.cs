9bf0b2707f8f1480db712d9b16de7b985efbbb29
facundo <00facuperez@gmail.com>
2025-10-12 12:13:34 -0300

    contexto y conexion base

---
+++ proyecto4programacion/Data/PrometeoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace proyecto4programacion.Data
+{
+    public class PrometeoContext : DbContext
+    {
+        public PrometeoContext(DbContextOptions<PrometeoContext> options) : base(options) { }
+    }
+}
---
+++ proyecto4programacion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyecto4programacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
